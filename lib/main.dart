ff9c39085d9ab0a6ea232e7313e153ff0fe5d6cb
Daniel <iovescu.dani@gmail.com>
2020-11-24 00:25:16 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(CurrencyConvertor());
 
-class MyApp extends StatelessWidget {
+class CurrencyConvertor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Convertor(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class Convertor extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ConvertorState createState() => _ConvertorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _ConvertorState extends State<Convertor> {
+  double rate = 4.5;
+  double convertedAmount = 0.0;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.k
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Currency convertor'),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Padding(
+          child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              TextField(
+                onChanged: (String value) {
+                  print(value.trim());
+                  setState(() {
+                    convertedAmount = double.parse(value) * rate;
+                  });
+                },
+                decoration: const InputDecoration(hintText: 'Amount'),
+                keyboardType: TextInputType.number,
+              ),
+              Center(
+                  child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        '$convertedAmount EUR',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      )))
+            ],
+          ),
+          padding: const EdgeInsets.all(10.0),
+        ));
   }
 }
